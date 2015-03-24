83af9c3e768c647f0236fd70dfa4dcd7196d235e
Dicksson Rammon Oliveira de Almeida <droa@cin.ufpe.br>
2015-03-24 19:47:04 -0300

    Adicionado mais exercicios da aula de 02

--- 20150324.hs
+++ 20150324.hs
@@ -49,6 +49,11 @@ isVertical ((x1, y1), (x2, y2))
  | x1 == x2 = True
  | otherwise = False
 
+pontoY :: Float -> Reta -> Float
+pontoY x r@((x1, y1), (x2, y2))
+ | isVertical r = error "line is vertical"
+ | otherwise = ((y2 - y1)/(x2 - x1)) * (x - x1) + y1
+
 {- Funções sobre o banco de dados - consultas -}
 type Pessoa = String
 type Livro = String
@@ -60,7 +65,7 @@ livros (x:xs) p
  | fst x == p = (snd x):livros xs p
  | otherwise = livros xs p
 
-{- using list comprehension 
+{- using list comprehension
 livros :: BancoDados -> Pessoa -> [Livro]
 livros xs pp = [l | (p, l) <- xs, pp == p]
 -}
@@ -71,10 +76,55 @@ emprestimos (x:xs) l
  | snd x == l = (fst x):emprestimos xs l
  | otherwise = emprestimos xs l
 
-{- using list comprehension 
+{- using list comprehension
 emprestimos :: BancoDados -> Livro -> [Pessoa]
 emprestimos xs ll = [p | (p, l) <- xs, ll == l]
 -}
+
+emprestado :: BancoDados -> Livro -> Bool
+emprestado [] _ = False
+emprestado (x:xs) l
+ | l == snd x = True
+ | otherwise = emprestado xs l
+
+{- using list comprehension
+emprestado xs ll = not (null [l | (p, l) <- xs, ll == l])
+-}
+
+qtdEmprestimos :: BancoDados -> Pessoa -> Int
+qtdEmprestimos [] _ = 0
+qtdEmprestimos (x:xs) p
+ | fst x == p = 1 + qtdEmprestimos xs p
+ | otherwise = qtdEmprestimos xs p
+
+{- using list comprehension
+qtdEmprestimos xs pp = length [p | (p, l) <- xs, pp == p]
+-}
+
+{- Funções sobre o banco de dados - atualizações -}
+emprestar :: BancoDados -> Pessoa -> Livro -> BancoDados
+emprestar [] p l = [(p, l)]	
+emprestar (x:xs) p l = x:(emprestar xs) p l
+
+devolver :: BancoDados -> Pessoa -> Livro -> BancoDados
+devolver [] p l = []
+devolver (x:xs) p l
+ | fst x == p && snd x == l = devolver xs p l
+ | otherwise = x:(devolver xs p l)
+
+{- using list comprehension
+devolver xs pp ll = [(p, l) | (p, l) <- xs, pp /= p || ll /= l]
+-}
+
+{- function 'membro' using list comprehension -}
+membro :: [Int] -> Int -> Bool
+membro xs xx = not (null [x | x <- xs, xx == x])
+
+{- Defina uma função que ordena uma lista de
+inteiros utilizando o algoritmo quick sort -}
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (a:as) = quicksort ([x | x <- as, x <= a]) ++ [a] ++ quicksort ([x | x <- as, x > a])
 
 baseExemplo :: BancoDados
 baseExemplo = 
@@ -82,3 +132,16 @@ baseExemplo =
 	("Andre", "Duna"),
 	("Fernando", "Jonathan Strange & Mr. Norrell"),
 	("Fernando", "A Game of Thrones")]
+
+{- Processamento de Texto -}
+getWord :: String -> String
+getWord (' ':xs) = []
+getWord (x:xs) = x:(getWord xs)
+
+dropWord :: String -> String
+dropWord (' ':xs) = ' ':xs
+dropWord (x:xs) = dropWord xs
+
+dropSpace :: String -> String
+dropSpace (' ':xs) = xs
+dropSpace (x:xs) = dropSpace xs

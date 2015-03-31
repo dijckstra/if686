11fcd0669ed61cc4dfc0aa456ae58cce18c69f7a
unknown <droa@g4c40.windows.cin.ufpe.br>
2015-03-31 11:01:49 -0300

    Adicionada questao 1 do Trabalho 4

--- 20150331.hs
+++ 20150331.hs
@@ -1,3 +1,18 @@
+{- Classes de tipos em Haskell são uma maneira de usar polimorfismo de
+sobrecarga, como existe em linguagens como Java. Pense com cuidado e
+explique quais s̃ão as vantagens e desvantagens de usar essa abordagem para
+polimorfismo de sobrecarga, em comparacao com a abordagem usada por Java.
+
+R: Classes de tipos são como interfaces que definem um comportamento. Se temos um tipo que faz parte da classe 
+estamos assumindo que ele suporta e implementa o comportamento oferecido pela classe.
+O polimorfismo de sobrecarga é utilizado quando temos funções com o mesmo nome, mas definições distintas.
+Em Java podemos definir metodos com o mesmo nome, porém com quantidade/tipo de parametros distintos.
+Em Haskell as classes de tipo permitem definir diferentes tipos para uma função, facilitando sua definição (o uso da sobrecarga). -}
+
+
+{- Implemente uma função que devolva o N-ésimo
+número da sequência look-and-say. -}
+
 -- Given a string, "say" what's in the string.
 say :: [Char] -> [Char]
 say [] = []

a75edd30ac2f9df1779dce549f06bebd36d959ba
Dicksson Rammon Oliveira de Almeida <droa@cin.ufpe.br>
2015-05-11 22:01:47 -0300

    Adicionado exercicios da aula

--- 20150505.hs
+++ 20150505.hs
@@ -3,7 +3,7 @@ import System.IO
 import Data.Char (isLetter, isSpace, toUpper)
 import Data.Maybe (catMaybes)
 
-{-------------------- TRABALHO 11 --------------------}
+{---------------------------------------- TRABALHO 11 ----------------------------------------}
 discard :: String -> Maybe String
 discard [] = Just []
 discard (a:as) = do
@@ -39,3 +39,41 @@ main = do
 	result <- return (split (return (return line >>= discard) >>= toUpperCase))
 	mapM_ putStrLn result
 	main
+
+{---------------------------------------- EXERCÍCIOS ----------------------------------------}
+type Stack = [Int]
+type Queue = [Int]
+
+{- Questão 3 -}
+newtype StateTransformer a = State {runState :: (Stack, Queue) -> (a, (Stack, Queue))}
+
+instance Monad StateTransformer where
+	return x = State (\(st, qu) -> (x, (st, qu)))
+	(>>=) (State rS) (f) = State $ 
+	 \(st, qu) -> let
+	 (x, (newStack, newQueue)) = rS (st, qu)
+	 (State newRS) = f x
+	 in newRS (newStack, newQueue)
+
+pop :: StateTransformer Int
+pop = State (\(st, qu) -> if st == [] then error "stack is empty" else (head st, (tail st, qu)))
+
+push :: Int -> StateTransformer ()
+push v = State (\(st, qu) -> ((), (v:st, qu)))
+
+insert :: Int -> StateTransformer ()
+insert v = State (\(st, qu) -> ((), (st, v:qu)))
+
+remove :: StateTransformer Int
+remove = State (\(st, qu) -> if qu == [] then error "queue is empty" else (last qu, (st, init qu)))
+
+{- Questão 2 -}
+newtype State stateType a = State {runState :: stateType -> (a, stateType)}
+
+instance Monad (State stateType) where
+	return x = State (\stateType -> (x, stateType))
+	(>>=) (State rS) (f) = State $ 
+	 \stateType -> let
+	 (x, newState) = rS newState
+	 (State newRS) = f x
+	 in newRS newState
